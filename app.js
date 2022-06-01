77615f51aacff322ed467d8b8e3f1300864b1065
Rilesh khatri <khatririlesh8@gmail.com>
2022-06-01 14:48:38 +0545

    Add environment variables

---
+++ app.js
@@ -0,0 +1,83 @@
+//jshint esversion:6
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+//plugins must be defined before mongoose model
+userSchema.plugin(encrypt, {secret: process.env.SECRET_KEY, encryptedFields: ["password"]});
+
+const User = mongoose.model("User", userSchema);
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.post("/login", function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({email: username}, function(err, foundUser){
+    if(!err){
+      if(foundUser){
+        if(foundUser.password === password){
+          console.log(foundUser.password);
+          res.render("secrets");
+        }
+      }
+    }else{
+      res.send(err);
+      console.log(err);
+    }
+  });
+});
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.post("/register", function(req, res){
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function(err){
+    if(!err){
+      res.render("secrets");
+    }else{
+      res.send(err);
+      console.log(err);
+    }
+  });
+});
+
+
+
+
+
+
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000");
+})

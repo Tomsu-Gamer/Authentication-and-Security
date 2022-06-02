06d1b190ee2a521f35550bc4422c2abac771047c
Rilesh khatri <khatririlesh8@gmail.com>
2022-06-02 13:43:25 +0545

    add bcrypt function

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 //jshint esversion:6
-require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
 
+const saltRounds = 10;
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
@@ -24,48 +24,52 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/login", function(req, res){
+app.get("/login", function(req, res) {
   res.render("login");
 });
 
-app.post("/login", function(req, res){
+app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
-  User.findOne({email: username}, function(err, foundUser){
-    if(!err){
-      if(foundUser){
-        if(foundUser.password === password){
-          console.log(foundUser.password);
-          res.render("secrets");
-        }
+  const password = req.body.password;
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
+    if (!err) {
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if(result === true){
+            res.render("secrets");
+          }
+        });
       }
-    }else{
+    } else {
       res.send(err);
       console.log(err);
     }
   });
 });
 
-app.get("/register", function(req, res){
+app.get("/register", function(req, res) {
   res.render("register");
 });
 
-app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
-  newUser.save(function(err){
-    if(!err){
-      res.render("secrets");
-    }else{
-      res.send(err);
-      console.log(err);
-    }
+app.post("/register", function(req, res) {
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err) {
+      if (!err) {
+        res.render("secrets");
+      } else {
+        console.log(err);
+      }
+    });
   });
 });
 
@@ -74,7 +78,6 @@ app.post("/register", function(req, res){
 
 
 
-
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server started on port 3000");
 })
